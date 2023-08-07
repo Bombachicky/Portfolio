27dd3007c697e6ae00d717bd13e0fd3de4ed5af1
Bombachicky <kristianjeanmichel@gmail.com>
2023-08-07 15:29:51 -0400

    more descriptions

--- components/Projects.vue
+++ components/Projects.vue
@@ -2,11 +2,13 @@
     <div id="projects"  class="min-h-[100px]"></div>
     <div class="flex justify-center text-[#08e67e] text-6xl mb-16 font-cocomat" v-motion-slide-visible-once-bottom>Projects</div>
     <div class="flex flex-col items-center">
-        <div class="grid grid-cols-1 lg:grid-cols-3 grid-rows-3 mt-10 gap-12 px-8 font-cocomat">
+        <div class="grid grid-cols-1 lg:grid-cols-3 grid-rows-3 mt-10 gap-12 px-8 font-cocomatreg">
             <button class="button" role="button" @mouseover="hoveredButton = 'button1'" @mouseleave="hoveredButton = ''" v-motion-slide-visible-once-bottom>
                 <a href="https://github.com/Bombachicky/MajorMentor">
                     <div class="text-2xl text-[#08e67e]" v-if="hoveredButton != 'button1'">Major Mentor</div>
                     <div v-else class="text-black text-2xl">Major Mentor</div>
+                    <div class="text-md">A web application that
+                                        generates customized academic guidance for students in the form of downloadable PDFs using the OpenAI API</div>
                     <div class="grid grid-cols-2">
                         <AnimatedText :text="'React'" v-if="hoveredButton != 'button1'" />
                         <div v-else class="text-black">React</div>
@@ -30,6 +32,7 @@
             <button class="button" role="button" @mouseover="hoveredButton = 'button2'" @mouseleave="hoveredButton = ''" v-motion-slide-visible-once-bottom>
                 <a href="https://github.com/Bombachicky/Pepsi">
                     <div class="text-2xl">Pepsi Website Recreation</div>
+                    <div class="text-md">A responsive and visually appealing Pepsi website replica with a robust REST backend</div>
                     <div class="grid grid-cols-2">
                         <AnimatedText :text="'React'" v-if="hoveredButton != 'button2'" />
                         <div v-else class="text-black">React</div>
@@ -51,6 +54,7 @@
             <button class="button" role="button" @mouseover="hoveredButton = 'button3'" @mouseleave="hoveredButton = ''" v-motion-slide-visible-once-bottom>
                 <a href="https://github.com/Bombachicky/PL-O-Compiler">
                     <div class="text-2xl">PL/O Compiler</div>
+                    <div class="text-md">Coded a compiler capable of translating a programming language known as PL/O into C language</div>
                     <div class="flex justify-center">
                         <AnimatedText :text="'C'" v-if="hoveredButton != 'button3'" />
                         <div v-else class="text-black">C</div>
@@ -61,6 +65,7 @@
             <button class="button" role="button" @mouseover="hoveredButton = 'button4'" @mouseleave="hoveredButton = ''" v-motion-slide-visible-once-bottom>
                 <a href="https://github.com/Bombachicky/Starbucks">
                 <div class="text-2xl">Starbucks Home Page</div>
+                <div class="text-md">A replica of the Starbucks website home page using HTML and CSS</div>
                     <div class="grid grid-cols-2">
                         <AnimatedText :text="'HTML'" v-if="hoveredButton != 'button4'" />
                         <div v-else class="text-black">HTML</div>
@@ -71,8 +76,9 @@
                 </a>
             </button>
             <button class="button" role="button" @mouseover="hoveredButton = 'button5'" @mouseleave="hoveredButton = ''" v-motion-slide-visible-once-bottom>
-                <a href="https://github.com/Bombachicky/Korobot">
+                <a href="https://github.com/Bombachicky/Korobot" >
                     <div class="text-2xl">World RPG Discord Bot</div>
+                    <div class="text-md">A Discord bot made to emulate a multiplayer turn-based strategy RPG game that I self-designed</div>
                     <div class="grid grid-cols-2">
                         <AnimatedText :text="'Discord.js'" v-if="hoveredButton != 'button5'" />
                         <div v-else class="text-black">Discord.js</div>
@@ -82,8 +88,6 @@
                         <div v-else class="text-black">Prisma</div>
                         <AnimatedText :text="'mySQL'" v-if="hoveredButton != 'button5'" />
                         <div v-else class="text-black">mySQL</div>
-                        <AnimatedText :text="'HTML'" v-if="hoveredButton != 'button5'" />
-                        <div v-else class="text-black">HTML</div>
                         <AnimatedText :text="'Cron'" v-if="hoveredButton != 'button5'" />
                         <div v-else class="text-black">Cron</div>
                         <AnimatedText :text="'GraphQL'" v-if="hoveredButton != 'button5'" />
@@ -95,6 +99,7 @@
             <button class="button" role="button" @mouseover="hoveredButton = 'button6'" @mouseleave="hoveredButton = ''" v-motion-slide-visible-once-bottom>
                 <a href="https://github.com/Bombachicky/Portfolio">
                 <div class="text-2xl">Portfolio Website</div>
+                <div class="text-md font-cocomatreg">Developed a dynamic personal portfolio using Vue, Nuxt, Tailwind, Three.js, and Vercel showcasing projects and professional skills</div>
                     <div class="grid grid-cols-2">
                         <AnimatedText :text="'Nuxt'" v-if="hoveredButton != 'button6'" />
                         <div v-else class="text-black">Nuxt</div>
@@ -113,6 +118,7 @@
             <button class="button" role="button" @mouseover="hoveredButton = 'button7'" @mouseleave="hoveredButton = ''" v-motion-slide-visible-once-bottom>
                 <a href="https://github.com/Bombachicky/SpaceTycoon">
                 <div class="text-2xl">Space Tycoon</div>
+                <div class="text-md">A game meant to simulate the process of managing a rocket launch station for future resource managers in today's launch sites</div>
                     <div class="grid grid-cols-2">
                         <AnimatedText :text="'Unity'" v-if="hoveredButton != 'button7'" />
                         <div v-else class="text-black">Unity</div>
